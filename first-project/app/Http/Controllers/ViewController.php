1d7c95b555c11eca33e22567ba0b5eb9452bc1ff
ohahiduzzaman1998 <ohahiduzzaman1998gmail.com>
2022-02-15 13:20:58 +0600

    laravel

---
+++ first-project/app/Http/Controllers/ViewController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ViewController extends Controller
+{
+    public function index()
+    {
+        return 'hello world';
+    }
+    public function bitm()
+    {
+        return 'hello bitm';
+    }
+}
---
+++ first-project/resources/views/demo.blade.php
@@ -0,0 +1,3 @@
+<h1>this is heading</h1>
+
+
